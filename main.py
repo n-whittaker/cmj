f19b8e22b849e20ad9b86f8aa72f1a4165b8aeaf
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2025-02-28 16:43:53 +0000

    Nearly all variables

--- main.py
+++ main.py
@@ -4,26 +4,45 @@ import numpy as np
 import os
 from read_c3d import read_c3d
 
-# Setting the directory to run through
-root_dir = "/Users/nick/Documents/University/Research Project/HT/AB 127331 Retest"
-
 # Set display option to show all columns and prevent truncation
 pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_seq_items', None)
 
+# Setting the directory to run through
+root_dir = "/Users/nick/Documents/University/Research Project/HT/AB 127331 Retest"
+
+# Checking ENF file for injured side
+enf_file_path = None
+for filename in os.listdir(root_dir):
+    if filename.endswith(".enf"):
+        enf_file_path = os.path.join(root_dir, filename)
+        break
+
+# Check if an ENF file was found and process it
+if enf_file_path:
+    with open(enf_file_path, "r") as file:
+        for line in file:
+            if line.startswith("INJURY="):
+                injured_side = line.split("=")[1].strip()
+                break
+    print("Injured side:", injured_side)
+else:
+    print("No ENF file found in the directory.")
+
 
 # Get CMJ c3d files
 def getFiles():
     files = []
-    # Walk through directory, check for "new session", go through files in new session, return CMJ c3ds
-    # list.
+    # Walk through directory, check for "New Session" and ENF file.
     for (dirpath, dirnames, filenames) in os.walk(root_dir):
+
+        # Look for CMJ c3d files in "New Session"
         if "New Session" in dirnames:
             new_session_path = os.path.join(dirpath, "New Session")
             for session_file in os.listdir(new_session_path):
-                if session_file.endswith(f".c3d") and "CMJ" in session_file and "SL" not in session_file:
+                if session_file.endswith(".c3d") and "CMJ" in session_file and "SL" not in session_file:
                     file_path = os.path.join(new_session_path, session_file)
                     files.append(file_path)
     return files
@@ -129,70 +148,151 @@ for file in cmjs:
     # Variable calculations
     # -------------------------
 
-    # Jump height
+    # Jump height sing the impulse-momentum equation
     g = 9.81  # Gravity
     v_takeoff = com_vel_z.loc[con_end]  # Extract velocity at takeoff
-    # Calculate jump height using the impulse-momentum equation
-    jump_height = (v_takeoff ** 2) / (2 * g)
-    jump_height_cm = jump_height * 100
-    jump_height_cm_rounded = round(jump_height_cm, 2)  # Round to 2 decimal places
-
-    # Peak GRFV Left + Right
-    peak_GRFv_Left = round(grfDF["Fz2"].max(), 2)
-    peak_GRFv_Right = round(grfDF["Fz1"].max(), 2)
-
-    # Peak HKA Extension Moments
-    peak_H_moment_Left = round(mocapDF["LHipMoment_x"].max(), 2)
-    peak_H_moment_Right = round(mocapDF["RHipMoment_x"].max(), 2)
-    peak_K_moment_Left = round(mocapDF["LKneeMoment_x"].max(), 2)
-    peak_K_moment_Right = round(mocapDF["RKneeMoment_x"].max(), 2)
-    peak_A_moment_Left = round(mocapDF["LAnkleMoment_x"].max(), 2)
-    peak_A_moment_Right = round(mocapDF["RAnkleMoment_x"].max(), 2)
-
-    # Peak HKA Flexion Angles
-    peak_H_angle_Left = round(mocapDF["LHipAngles_x"].max(), 2)
-    peak_H_angle_Right = round(mocapDF["RHipAngles_x"].max(), 2)
-    peak_K_angle_Left = round(mocapDF["LKneeAngles_x"].max(), 2)
-    peak_K_angle_Right = round(mocapDF["RKneeAngles_x"].max(), 2)
-    peak_A_angle_Left = round(mocapDF["LAnkleAngles_x"].max(), 2)
-    peak_A_angle_Right = round(mocapDF["RAnkleAngles_x"].max(), 2)
+    jump_height_cm = round(((v_takeoff ** 2) / (2 * g)) * 100, 2)
 
     # Impulse using trapezoid rule
-    # Time step based on  sampling rate
-    dt = 1.0 / sampling_rate
-    # Eccentric deceleration phase
+    dt = 1.0 / sampling_rate  # Time step based on  sampling rate
     impulse_ED_right = round(np.trapz(grfDF["Fz1"][ED_start:ED_end], dx=dt), 2)
     impulse_ED_left = round(np.trapz(grfDF["Fz2"][ED_start:ED_end], dx=dt), 2)
-    # Concentric phase
     impulse_con_right = round(np.trapz(grfDF["Fz1"][ED_end:con_end], dx=dt), 2)
     impulse_con_left = round(np.trapz(grfDF["Fz2"][ED_end:con_end], dx=dt), 2)
-    # Landing phase:
     impulse_landing_right = round(np.trapz(grfDF["Fz1"][landing_start:landing_end], dx=dt), 2)
     impulse_landing_left = round(np.trapz(grfDF["Fz2"][landing_start:landing_end], dx=dt), 2)
 
+    # Peak GRFv
+    peak_GRFv_ED_Right = round(grf["Fz1"][ED_start:ED_end].max(), 2)
+    peak_GRFv_con_Right = round(grf["Fz1"][con_start:con_end].max(), 2)
+    peak_GRFv_landing_Right = round(grf["Fz1"][landing_start:landing_end].max(), 2)
+    peak_GRFv_ED_Left = round(grf["Fz2"][ED_start:ED_end].max(), 2)
+    peak_GRFv_con_Left = round(grf["Fz2"][con_start:con_end].max(), 2)
+    peak_GRFv_landing_Left = round(grf["Fz2"][landing_start:landing_end].max(), 2)
+
+    # Peak Hip Flexion Angles
+    peak_hip_angle_ED_Left = round(mocapDF["LHipAngles_x"][landing_start:landing_end].max(), 2)
+    peak_hip_angle_con_Left = round(mocapDF["LHipAngles_x"][con_start:con_end].max(), 2)
+    peak_hip_angle_landing_Left = round(mocapDF["LHipAngles_x"][landing_start:landing_end].max(), 2)
+    peak_hip_angle_ED_Right = round(mocapDF["RHipAngles_x"][landing_start:landing_end].max(), 2)
+    peak_hip_angle_con_Right = round(mocapDF["RHipAngles_x"][con_start:con_end].max(), 2)
+    peak_hip_angle_landing_Right = round(mocapDF["RHipAngles_x"][landing_start:landing_end].max(), 2)
+
+    # Peak Knee Flexion Angles
+    peak_knee_angle_ED_Left = round(mocapDF["LKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_knee_angle_con_Left = round(mocapDF["LKneeAngles_x"][con_start:con_end].max(), 2)
+    peak_knee_angle_landing_Left = round(mocapDF["LKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_knee_angle_ED_Right = round(mocapDF["RKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_knee_angle_con_Right = round(mocapDF["RKneeAngles_x"][con_start:con_end].max(), 2)
+    peak_knee_angle_landing_Right = round(mocapDF["RKneeAngles_x"][landing_start:landing_end].max(), 2)
+
+    # Peak Ankle Flexion Angles
+    peak_ankle_angle_ED_Left = round(mocapDF["LKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_angle_con_Left = round(mocapDF["LKneeAngles_x"][con_start:con_end].max(), 2)
+    peak_ankle_angle_landing_Left = round(mocapDF["LKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_angle_ED_Right = round(mocapDF["RKneeAngles_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_angle_con_Right = round(mocapDF["RKneeAngles_x"][con_start:con_end].max(), 2)
+    peak_ankle_angle_landing_Right = round(mocapDF["RKneeAngles_x"][landing_start:landing_end].max(), 2)
+
+    # Peak Hip Extension Moment
+    peak_hip_moment_ED_Left = round(mocapDF["LHipMoment_x"][landing_start:landing_end].max(), 2)
+    peak_hip_moment_con_Left = round(mocapDF["LHipMoment_x"][con_start:con_end].max(), 2)
+    peak_hip_moment_landing_Left = round(mocapDF["LHipMoment_x"][landing_start:landing_end].max(), 2)
+    peak_hip_moment_ED_Right = round(mocapDF["RHipMoment_x"][landing_start:landing_end].max(), 2)
+    peak_hip_moment_con_Right = round(mocapDF["RHipMoment_x"][con_start:con_end].max(), 2)
+    peak_hip_moment_landing_Right = round(mocapDF["RHipMoment_x"][landing_start:landing_end].max(), 2)
+
+    # Peak Knee Extension Moment
+    peak_knee_moment_ED_Left = round(mocapDF["LKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_knee_moment_con_Left = round(mocapDF["LKneeMoment_x"][con_start:con_end].max(), 2)
+    peak_knee_moment_landing_Left = round(mocapDF["LKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_knee_moment_ED_Right = round(mocapDF["RKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_knee_moment_con_Right = round(mocapDF["RKneeMoment_x"][con_start:con_end].max(), 2)
+    peak_knee_moment_landing_Right = round(mocapDF["RKneeMoment_x"][landing_start:landing_end].max(), 2)
+
+    # Peak Knee Extension Moment
+    peak_ankle_moment_ED_Left = round(mocapDF["LKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_moment_con_Left = round(mocapDF["LKneeMoment_x"][con_start:con_end].max(), 2)
+    peak_ankle_moment_landing_Left = round(mocapDF["LKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_moment_ED_Right = round(mocapDF["RKneeMoment_x"][landing_start:landing_end].max(), 2)
+    peak_ankle_moment_con_Right = round(mocapDF["RKneeMoment_x"][con_start:con_end].max(), 2)
+    peak_ankle_moment_landing_Right = round(mocapDF["RKneeMoment_x"][landing_start:landing_end].max(), 2)
+
+    # DESCRIPTIVE VARIABLES INTO A DICTIONARY
+    # var_outputs = {
+    #     "Jump Height (cm)": jump_height_cm,
+    #     "Impulse ED Right (N·s)": impulse_ED_right,
+    #     "Impulse ED Left (N·s)": impulse_ED_left,
+    #     "Impulse Con Right (N·s)": impulse_con_right,
+    #     "Impulse Con Left (N·s)": impulse_con_left,
+    #     "Impulse Landing Right (N·s)": impulse_landing_right,
+    #     "Impulse Landing Left (N·s)": impulse_landing_left,
+    #     "Peak GRFv ED Right (N)": peak_GRFv_ED_Right,
+    #     "Peak GRFv Con Right (N)": peak_GRFv_con_Right,
+    #     "Peak GRFv Landing Right (N)": peak_GRFv_landing_Right,
+    #     "Peak GRFv ED Left (N)": peak_GRFv_ED_Left,
+    #     "Peak GRFv Con Left (N)": peak_GRFv_con_Left,
+    #     "Peak GRFv Landing Left (N)": peak_GRFv_landing_Left,
+    #     "Peak Hip Angle ED Left (°)": peak_hip_angle_ED_Left,
+    #     "Peak Hip Angle Con Left (°)": peak_hip_angle_con_Left,
+    #     "Peak Hip Angle Landing Left (°)": peak_hip_angle_landing_Left,
+    #     "Peak Hip Angle ED Right (°)": peak_hip_angle_ED_Right,
+    #     "Peak Hip Angle Con Right (°)": peak_hip_angle_con_Right,
+    #     "Peak Hip Angle Landing Right (°)": peak_hip_angle_landing_Right,
+    #     "Peak Knee Angle ED Left (°)": peak_knee_angle_ED_Left,
+    #     "Peak Knee Angle Con Left (°)": peak_knee_angle_con_Left,
+    #     "Peak Knee Angle Landing Left (°)": peak_knee_angle_landing_Left,
+    #     "Peak Knee Angle ED Right (°)": peak_knee_angle_ED_Right,
+    #     "Peak Knee Angle Con Right (°)": peak_knee_angle_con_Right,
+    #     "Peak Knee Angle Landing Right (°)": peak_knee_angle_landing_Right,
+    #     "Peak Ankle Angle ED Left (°)": peak_ankle_angle_ED_Left,
+    #     "Peak Ankle Angle Con Left (°)": peak_ankle_angle_con_Left,
+    #     "Peak Ankle Angle Landing Left (°)": peak_ankle_angle_landing_Left,
+    #     "Peak Ankle Angle ED Right (°)": peak_ankle_angle_ED_Right,
+    #     "Peak Ankle Angle Con Right (°)": peak_ankle_angle_con_Right,
+    #     "Peak Ankle Angle Landing Right (°)": peak_ankle_angle_landing_Right,
+    #     "Peak Hip Moment ED Left (Nm)": peak_hip_moment_ED_Left,
+    #     "Peak Hip Moment Con Left (Nm)": peak_hip_moment_con_Left,
+    #     "Peak Hip Moment Landing Left (Nm)": peak_hip_moment_landing_Left,
+    #     "Peak Hip Moment ED Right (Nm)": peak_hip_moment_ED_Right,
+    #     "Peak Hip Moment Con Right (Nm)": peak_hip_moment_con_Right,
+    #     "Peak Hip Moment Landing Right (Nm)": peak_hip_moment_landing_Right,
+    #     "Peak Knee Moment ED Left (Nm)": peak_knee_moment_ED_Left,
+    #     "Peak Knee Moment Con Left (Nm)": peak_knee_moment_con_Left,
+    #     "Peak Knee Moment Landing Left (Nm)": peak_knee_moment_landing_Left,
+    #     "Peak Knee Moment ED Right (Nm)": peak_knee_moment_ED_Right,
+    #     "Peak Knee Moment Con Right (Nm)": peak_knee_moment_con_Right,
+    #     "Peak Knee Moment Landing Right (Nm)": peak_knee_moment_landing_Right,
+    #     "Peak Ankle Moment ED Left (Nm)": peak_ankle_moment_ED_Left,
+    #     "Peak Ankle Moment Con Left (Nm)": peak_ankle_moment_con_Left,
+    #     "Peak Ankle Moment Landing Left (Nm)": peak_ankle_moment_landing_Left,
+    #     "Peak Ankle Moment ED Right (Nm)": peak_ankle_moment_ED_Right,
+    #     "Peak Ankle Moment Con Right (Nm)": peak_ankle_moment_con_Right,
+    #     "Peak Ankle Moment Landing Right (Nm)": peak_ankle_moment_landing_Right
+    # }
+
     # Asymmetry Indices
-    # AI = ((Dominant vs Non-Dominant) / (Maximum of dominant and non-dominant)) * 100
-    # May need to look at spreadsheet to determine dominant legs
+    # AI = ((Uninjured vs ACLR limb) / (whichever is biggest)) * 100
+    # Positive number means uninjured performed BETTER than injured
+    def AI_calc(right, left, inj_side):
+        asymIndex = None
+
+        if inj_side == "Right":
+            asymIndex = ((left - right) / max(right, left)) * 100
+        elif inj_side == "Left":
+            asymIndex = ((right - left) / max(right, left)) * 100
+
+        return asymIndex
+
+
+
+
 
     # -------------------------
     # Print Variables
     # -------------------------
     print("Trial number: ", trial_number)
-    print("Jump Height (cm): ", jump_height_cm_rounded)
-    print("Peak GRFv Left (N): ", peak_GRFv_Left)
-    print("Peak GRFv Right (N): ", peak_GRFv_Right)
-    print("Peak H Extension Moment (N): LEFT: ", peak_H_moment_Left, " RIGHT: ", peak_H_moment_Right)
-    print("Peak K Extension Moment (N): LEFT: ", peak_K_moment_Left, " RIGHT: ", peak_K_moment_Right)
-    print("Peak A Extension Moment (N): LEFT: ", peak_A_moment_Left, " RIGHT: ", peak_A_moment_Right)
-    print("Peak H Flexion Angle (N): LEFT: ", peak_H_angle_Left, " RIGHT: ", peak_H_angle_Right)
-    print("Peak K Flexion Angle (N): LEFT: ", peak_K_angle_Left, " RIGHT: ", peak_K_angle_Right)
-    print("Peak A Flexion Angle (N): LEFT: ", peak_A_angle_Left, " RIGHT: ", peak_A_angle_Right)
-    print("ED Impulse (N.s) LEFT:", impulse_ED_left, " RIGHT:", impulse_ED_right)
-    print("CON Impulse (N.s) LEFT:", impulse_con_left, " RIGHT:", impulse_con_right)
-    print("Landing Impulse (N.s) LEFT:", impulse_landing_left, " RIGHT:", impulse_landing_right)
-
+    print(AI_calc(peak_knee_moment_con_Right, peak_knee_moment_con_Left, injured_side))
     print("")  # Spacer for readability in the terminal
 
     trial_number += 1  # iterate trial number
-
-
